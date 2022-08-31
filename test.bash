4557224d563389c4c3eef0619eb540e4b3c8e603
quorise <daison9200reply@gmail.com>
2022-08-31 17:05:40 +0900

    test make

--- test.bash
+++ test.bash
@@ -1,4 +1,21 @@
-#!/bin/bash
+#!/bin/bash 
+# SPDX-FileCopyrightText: 2022 quorise
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+        echo NG at line $1
+        res=1
+}
+
+res=0
 
 
-seq 5 | ./plus
+### IO test ###
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng $LINENO
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
+
